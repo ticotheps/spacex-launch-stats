6d7ac1c64f9df6ca861071b57edc3a23a546980e
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-04-25 01:09:59 -0400

    Creates another field in the RootQuery called 'launch'

--- schema.js
+++ schema.js
@@ -44,6 +44,19 @@ const RootQuery = new GraphQLObjectType({
 					.then((res) => res.data);
 			},
 		},
+		launch: {
+			type: LaunchType,
+			args: {
+				flight_number: { type: GraphQLInt },
+			},
+			resolve(parent, args) {
+				return axios
+					.get(
+						`https://api.spacexdata.com/v3/launches/${args.flight_number}`
+					)
+					.then((res) => res.data);
+			},
+		},
 	},
 });
 
