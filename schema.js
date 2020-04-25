4ec6961642ee09f64386bc9bf6d8b25eedf92d6c
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-04-25 01:01:02 -0400

    Creates a Root Query in the GraphQL schema

--- schema.js
+++ schema.js
@@ -1,8 +1,12 @@
+const axios = require('axios');
+
 const {
 	GraphQLObjectType,
 	GraphQLInt,
 	GraphQLString,
 	GraphQLBoolean,
+	GraphQLList,
+	GraphQLSchema,
 } = require('graphql');
 
 // Launch Type
@@ -26,4 +30,24 @@ const RocketType = new GraphQLObjectType({
 		rocket_name: { type: GraphQLString },
 		rocket_type: { type: GraphQLString },
 	}),
+});
+
+// Root Query
+const RootQuery = new GraphQLObjectType({
+	name: 'RootQueryType',
+	fields: {
+		launches: {
+			type: new GraphQLList(LaunchType),
+			resolve(parent, args) {
+				return axios
+					.get('https://api.spacexdata.com/v3/launches/')
+					.then((res) => res.data);
+			},
+		},
+	},
+});
+
+// Also takes in any mutations if we had any
+module.exports = new GraphQLSchema({
+	query: RootQuery,
 });
