e09503c9097003f4c3ef8d234678e2e4f4868c40
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-04-25 00:53:41 -0400

    Creates a 'LaunchType' in schema for graphQL

---
+++ schema.js
@@ -0,0 +1,19 @@
+const {
+	GraphQLObjectType,
+	GraphQLInt,
+	GraphQLString,
+	GraphQLBoolean,
+} = require('graphql');
+
+// Launch Type
+const LaunchType = new GraphQLObjectType({
+	name: 'Launch',
+	fields: () => ({
+		flight_number: { type: GraphQLInt },
+		mission_name: { type: GraphQLString },
+		launch_year: { type: GraphQLString },
+		launch_date_local: { type: GraphQLString },
+		launch_success: { type: GraphQLBoolean },
+		rocket: { type: RocketType },
+	}),
+});
